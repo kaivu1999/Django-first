199e13d4e943488724380ca3304ce6a8ded643a8
kaivalya <cs1170339@cse.iitd.ac.in>
2018-08-01 01:12:45 +0530

    tutorial completed

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,9 @@ from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 from django.shortcuts import redirect
+from django.http import HttpResponseRedirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 
 def post_list(request):
@@ -19,7 +22,7 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            
             post.save()
             return redirect('post_detail',pk=post.pk)
             
@@ -35,9 +38,22 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})    
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render (request, 'blog/post_draft_list.html',{'posts' :posts})
+
+def post_publish(request , pk):
+    post = get_object_or_404(Post , pk = pk)
+    post.publish()
+    return redirect('post_detail' , pk=pk) 
+
+def post_remove(request , pk):
+    post =  get_object_or_404(Post , pk= pk)
+    post.delete()
+    return redirect ('post_list')
